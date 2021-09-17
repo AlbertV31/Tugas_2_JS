ebc32fd5fdc4bcfe9c7ef43067b63d7d3e7d6c9b
Albert <albert.bagil@gmail.com>
2021-09-17 15:45:30 +0700

    Tugas 2 Javascript
    Tugas 2 Javascript untuk dikumpulkan ke Niomic

---
+++ Tugas2.js
@@ -0,0 +1,11 @@
+var nama = "Albert";
+var umur = 28;
+var menikah = false;
+var anak = null;
+var angka = [30,100,12,50,9];
+
+console.log(nama);
+console.log(umur);
+console.log(menikah);
+console.log(anak);
+console.log(angka);
